b4c4f6a58e292257de0c5f631baeadebbd198eb9
1kuzus <iikuzus@outlook.com>
2024-05-02 16:02:54 +0800

    change to public repo

--- caslogin/encrypt.py
+++ caslogin/encrypt.py
@@ -6,8 +6,6 @@ def encrypt(username, password, public_key):
     root_dir = os.path.dirname(__file__)
     with open(os.path.join(root_dir, "encrypt.js"), "r") as js_file:
         jscode = js_file.read()
-    jsencrypt_abspath = os.path.join(root_dir, "jsencrypt").replace("\\", "/")
-    jscode = re.sub(r"require\('\./jsencrypt'\)", f"require('{jsencrypt_abspath}')", jscode)
     jscode = re.sub(r"setPublicKey\('(.*?)'\)", f"setPublicKey('{public_key}')", jscode)
     encrypt_js = execjs.compile(jscode)
     ul = encrypt_js.call("strEnc", username)
